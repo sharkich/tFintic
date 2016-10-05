e6a9be8e550ed6190ec283442dfdd74f191871aa
Artem Koziar <a@temich.in.ua>
2016-10-05 23:59:25 +0300

    firebase items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,13 @@ export class AppComponent {
   items: FirebaseListObservable<any[]>;
 
   constructor(private angularFire: AngularFire) {
-    this.items = angularFire.database.list('items');
+    this.items = angularFire.database.list('/items');
     this.angularFire.auth.subscribe((auth) => {
       this.auth = auth;
     });
   }
+
+  // auth
 
   isAuth(): boolean {
     return !!(this.auth && this.auth.uid);
@@ -28,5 +30,20 @@ export class AppComponent {
 
   logout() {
     this.angularFire.auth.logout();
+  }
+
+  // items
+
+  addItem(newName: string) {
+    this.items.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.items.update(key, { text: newText });
+  }
+  deleteItem(key: string) {
+    this.items.remove(key);
+  }
+  deleteEverything() {
+    this.items.remove();
   }
 }
