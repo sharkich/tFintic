443778fc1b97fa5f85f344ed6ea13467d44ac968
Temich Koziar <a@temich.in.ua>
2016-10-25 09:51:56 +0300

    icons + auth button at footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AngularFire} from 'angularfire2';
+import {AuthService} from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private angularFire: AngularFire, // for template
+              private authService: AuthService) {
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
 }
